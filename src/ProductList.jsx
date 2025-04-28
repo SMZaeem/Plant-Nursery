36fd0fa04b46e2054aad256c47fa5584c5c57736
SMZaeem <zaeemdhoniline711@gmail.com>
2025-04-28 14:32:48 +0530

    changed background and build added

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -276,8 +276,8 @@ function ProductList({ onHomeClick }) {
                         <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                         <a href="/" onClick={(e) => handleHomeClick(e)}>
                             <div>
-                                <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
-                                <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
+                                <h3 style={{ color: 'white' }}>Plant Nursery</h3>
+                                <i style={{ color: 'white' }}>Where You Meet Nature</i>
                             </div>
                         </a>
                     </div>
